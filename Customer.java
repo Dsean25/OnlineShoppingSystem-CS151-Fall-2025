d552660dfd57f3947c12e3b05f9fa81fa1e6689b
Sweksha Shaw <swekshas123@gmail.com>
2025-10-10 04:45:03 -0700

    brought back customer by sweksha

--- Customer.java
+++ Customer.java
@@ -1,57 +1,67 @@
 public class Customer extends User {
+  private String name;
+  private String customerId;
   private ShoppingCart cart;
   private String paymentMethod;
 
-  public Customer(String userID,
-      String firstName,
-      String lastName,
-      String email,
-      String phoneNumber,
-      String address,
-      String storeName) {
-    super(userID, firstName, lastName, email, phoneNumber,address);  
-
-    cart = new ShoppingCart(userID);
+  public Customer(String name, String customerId){
+      //need to call on super();
+      this.name = name;
+      this.customerId = customerId;
+      cart = new ShoppingCart(customerId);
+  }
+  
+  //getters and setters
+  public void setCustomerID(String customerId) {
+      this.customerId = customerId;
   }
 
-  // setters
-
   public ShoppingCart getCart() {
-    return cart;
+      return cart;
   }
 
   public void setCart(ShoppingCart cart) {
-    this.cart = cart;
+      this.cart = cart;
   }
 
-  // getters
   public String getPaymentMethod() {
-    return paymentMethod;
+      return paymentMethod;
   }
 
   public void setPaymentMethod(String paymentMethod) {
-    this.paymentMethod = paymentMethod;
+      this.paymentMethod = paymentMethod;
   }
 
-  public void addProductToCart(Product p, int qty) {
-    cart.addToCart(p, qty);
-    // print statements are in addtocart
+  
+  public void addProduct(Product p, int qty){
+      cart.addProduct(p, qty);
+      //print statements are in addtocart
   }
 
-  public void removeProductFromCart(Product p, int qty) {
-    cart.removeFromCart(p, qty);
+  public void removeProduct(Product p, int qty){
+      cart.removeProduct(p, qty);
   }
 
-  public void placeOrder() {}
-
-  public void changePaymentMethod(String paymentmethod) {
-    setPaymentMethod(paymentMethod);
-    System.out.printf("Your payment method was changed to %s!%n", paymentmethod);
+  public void placeOrder(){
+  
   }
 
-  /*+ pending methods, according to UML:
+  public void calculateTotal(){
+      //in cart AND checkout??
+  }
+
+  public void changePaymentMethod(String paymentmethod){
+      setPaymentMethod(paymentMethod);
+      System.out.printf("Your payment method was changed to %s!%n", paymentmethod);
+  }
+
+     /*+ pending methods, according to UML: 
   removeProductFromCart(Product p): void
   changeProductQuantity(Product p, int qty):void
   */
+
+
+
+
 
 }
