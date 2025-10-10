4ef1a4dc04b5c69c826c0061c8643582c455703f
Toey <toeylui.dev@gmail.com>
2025-10-10 10:54:11 -0700

    Fixed syntax errors

--- Customer.java
+++ Customer.java
@@ -4,33 +4,32 @@ public class Customer extends User {
   private ShoppingCart cart;
   private String paymentMethod;
 
-  //getters and setters
+  // getters and setters
   public void setCustomerID(String customerId) {
-      this.customerId = customerId;
-=======
+    this.customerId = customerId;
+  }
+
   public Customer(String name, String customerId) {
     // need to call on super();
     super(customerId, name, "", name.toLowerCase() + "@gmail.com", "", "");
     this.name = name;
     this.customerId = customerId;
+  }
 
   public ShoppingCart getCart() {
-      return cart;
+    return cart;
   }
 
   public String getPaymentMethod() {
-      return paymentMethod;
+    return paymentMethod;
   }
 
   public void setPaymentMethod(String paymentMethod) {
-      this.paymentMethod = paymentMethod;
+    this.paymentMethod = paymentMethod;
   }
 
+  public void placeOrder() {}
 
-  public void placeOrder(
-  
-  ){
-  
   public void addProduct(Product p, int qty) {
     cart.addProduct(p, qty);
     // print statements are in addtocart
@@ -38,14 +37,10 @@ public class Customer extends User {
 
   public void removeProduct(Product p, int qty) {
     cart.removeProduct(p, qty);
-
   }
 
-  public void changePaymentMethod(String paymentmethod){
-      setPaymentMethod(paymentMethod);
-      System.out.printf("Your payment method was changed to %s!%n", paymentmethod);
+  public void changePaymentMethod(String paymentMethod) {
+    setPaymentMethod(paymentMethod);
+    System.out.printf("Your payment method was changed to %s!%n", paymentMethod);
   }
-
-
-
 }
