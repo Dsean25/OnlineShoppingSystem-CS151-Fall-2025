6123f392a4d7c25dac997d315d559a22163b1a32
sweksha-cloud <swekshas123@gmail.com>
2025-10-12 02:41:07 -0700

    removing calculate method in Operations.java

--- Operations.java
+++ Operations.java
@@ -2,6 +2,4 @@ public interface Operations {
   void addProduct(Product product, int quantity);
 
   void removeProduct(Product product, int quantity);
-
-  // not using this temporarily - void calculateTotal();
 }
